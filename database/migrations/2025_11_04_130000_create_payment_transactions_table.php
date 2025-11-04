a64f607be02af9a8bd492c1003372ea9fd7d0dbb
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-11-04 14:05:18 +0100

    add payment transactions migration file

---
+++ database/migrations/2025_11_04_130000_create_payment_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('payment_transactions', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->unsignedBigInteger('user_id');
+      $table->decimal('amount', 10, 2);
+      $table->string('provider_reference')->nullable();
+      $table->string('status', 50);
+      $table->timestamps();
+
+      // Indexes
+      $table->index('user_id');
+      $table->index('provider_reference');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('payment_transactions');
+  }
+};
