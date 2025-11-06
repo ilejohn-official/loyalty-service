cbc81b6dc8c8ca975180df8c6f32460b8fb22bef
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-11-06 16:37:58 +0100

    set up basic routes

---
+++ loyalty-program-fe/src/pages/AdminDashboard.tsx
@@ -0,0 +1,9 @@
+const AdminDashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+    </div>
+  );
+};
+
+export default AdminDashboard;
---
+++ loyalty-program-fe/src/pages/CustomerDashboard.tsx
@@ -0,0 +1,10 @@
+const CustomerDashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Customer Dashboard</h1>
+      
+    </div>
+  );
+};
+
+export default CustomerDashboard;
