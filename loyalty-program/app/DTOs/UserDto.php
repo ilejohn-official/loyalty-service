5c50200b134f4d6ebd27470ffe5bfdfae1b1ec90
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-11-06 21:33:50 +0100

    add nullable fields

--- loyalty-program/app/DTOs/UserDto.php
+++ loyalty-program/app/DTOs/UserDto.php
@@ -15,6 +15,12 @@ class UserDto implements Arrayable
 
     public ?string $email;
 
+    public ?string $paystack_recipient_code;
+
+    public ?string $flw_account_number;
+
+    public ?string $flw_bank_code;
+
     public array $attributes = [];
 
     public function __construct(array $data = [])
@@ -22,6 +28,9 @@ class UserDto implements Arrayable
         $this->id = isset($data['id']) ? (int) $data['id'] : null;
         $this->name = $data['name'] ?? null;
         $this->email = $data['email'] ?? null;
+        $this->paystack_recipient_code = $data['paystack_recipient_code'] ?? null;
+        $this->flw_account_number = $data['flw_account_number'] ?? null;
+        $this->flw_bank_code = $data['flw_bank_code'] ?? null;
         $this->attributes = $data;
     }
 
